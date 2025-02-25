75505396d6e99b1e5e091397cd22938f60c8a3c8
Anurag Verma <anurag1246verma2121@gmail.com>
2025-02-25 22:09:07 +0530

    Update App.jsx
    Signed-off-by: Anurag Verma <anurag1246verma2121@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Refer from "./components/refer/Refer";
 import ReferHero from "./components/refer/ReferHero";
 import ReferBenefits from "./components/benefits/ReferBenefits";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { HashRouter} from "react-router-dom";
 import Header from "./components/header/Header";
 import FooterComp from "./components/footer/FooterComp";
 import Home from "./pages/Home";
